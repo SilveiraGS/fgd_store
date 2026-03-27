15778eea2800f8bccc569ed6b5c1cadea68d4ca7
Guilherme Graeff <146678912+SilveiraGS@users.noreply.github.com>
2026-03-27 16:58:35 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'fgd_store'
+description 'Tablet NUI com suporte fgd_lib'
+version '1.0.0'
+lua54 'yes'
+
+dependencies {
+    'fgd_lib',
+    'vrp',
+    'target'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+ui_page 'web-side/index.html'
+
+files {
+    'web-side/index.html',
+    'web-side/assets/*',
+    'license.js',
+}
+
+client_scripts {
+    'client-side/*.lua'
+}
+
+server_scripts {
+    '@vrp/lib/Utils.lua',
+    'server-side/core.lua'
+}
